2a9d73a5dfbfacc5315b28caa0f7f172c4602958
8580 <42875380+8580@users.noreply.github.com>
2026-07-09 21:05:28 +0100

    Update __init__.py

--- mobius/embeddings/__init__.py
+++ mobius/embeddings/__init__.py
@@ -6,5 +6,6 @@
 
 from .protein_embeddings import ProteinEmbedding
 from .chemical_embeddings import ChemicalEmbedding
+from .cached_protein_embeddings import CachedProteinEmbedding
 
-__all__ = ['ProteinEmbedding', 'ChemicalEmbedding']
+__all__ = ['ProteinEmbedding', 'ChemicalEmbedding', 'CachedProteinEmbedding']
