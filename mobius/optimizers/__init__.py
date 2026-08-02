d6119b9349ab6da5face8062808f5d735c62bb6c
8580 <42875380+8580@users.noreply.github.com>
2026-08-02 20:44:12 +0100

    feat: patch in CachedSequenceGA

--- mobius/optimizers/__init__.py
+++ mobius/optimizers/__init__.py
@@ -5,6 +5,7 @@
 #
 
 from .genetic_algorithm import SequenceGA, RandomGA
+from .cached_sequence_ga import CachedSequenceGA
 from .pool import Pool
 
-__all__ = ['SequenceGA', 'RandomGA', 'Pool']
+__all__ = ['SequenceGA', 'CachedSequenceGA', 'RandomGA', 'Pool']
