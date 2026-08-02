9701d5a6770bb9c2d40fa0bd427fc0e241d19c11
8580 <42875380+8580@users.noreply.github.com>
2026-08-02 20:11:31 +0100

    featr: patch in CachedGPLLModel

--- mobius/surrogate_models/__init__.py
+++ mobius/surrogate_models/__init__.py
@@ -7,8 +7,9 @@
 from .dummy_model import DummyModel
 from .gaussian_process import GPModel
 from .gaussian_process_llm import GPLLModel
+from .cached_gaussian_process_llm import CachedGPLLModel  
 from .random_forest import RFModel
 from .gaussian_process_graph import GPGKModel
 from .gaussian_process_gnn import GPGNNModel
 
-__all__ = ['DummyModel', 'GPModel', 'GPLLModel', 'RFModel', 'GPGKModel', 'GPGNNModel']
+__all__ = ['DummyModel', 'GPModel', 'GPLLModel', 'CachedGPLLModel', 'RFModel', 'GPGKModel', 'GPGNNModel']
