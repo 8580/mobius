90e5945ea297f550b132f91e0cd0a8b989e5cacd
8580 <42875380+8580@users.noreply.github.com>
2026-08-12 09:35:14 +0100

    Update __init__.py

--- mobius/surrogate_models/__init__.py
+++ mobius/surrogate_models/__init__.py
@@ -11,5 +11,13 @@ from .cached_gaussian_process_llm import CachedGPLLModel
 from .random_forest import RFModel
 from .gaussian_process_graph import GPGKModel
 from .gaussian_process_gnn import GPGNNModel
+from .censoring import CensoringSpec, truncated_normal_moments, censored_normal_logpdf
+from .censored_gaussian_process import CensoredEMGPModel, SchmeeHahnGPModel, EMResult
+from .tobit_gaussian_process import TobitGPModel, CensoredGaussianLikelihood
 
-__all__ = ['DummyModel', 'GPModel', 'GPLLModel', 'CachedGPLLModel', 'RFModel', 'GPGKModel', 'GPGNNModel']
+__all__ = ['DummyModel', 'GPModel', 'GPLLModel', 'CachedGPLLModel', 'RFModel', 'GPGKModel', 'GPGNNModel',
+           'CensoringSpec', 'truncated_normal_moments', 'censored_normal_logpdf',
+           'CensoredEMGPModel', 'SchmeeHahnGPModel', 'EMResult',
+           'TobitGPModel', 'CensoredGaussianLikelihood']
+
+#__all__ = ['DummyModel', 'GPModel', 'GPLLModel', 'CachedGPLLModel', 'RFModel', 'GPGKModel', 'GPGNNModel']
