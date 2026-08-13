84afb55f11136f70599b17cd778566910236fa9e
8580 <42875380+8580@users.noreply.github.com>
2026-08-13 21:59:23 +0100

    Update __init__.py

--- mobius/surrogate_models/__init__.py
+++ mobius/surrogate_models/__init__.py
@@ -11,5 +11,6 @@ from .cached_gaussian_process_llm import CachedGPLLModel
 from .random_forest import RFModel
 from .gaussian_process_graph import GPGKModel
 from .gaussian_process_gnn import GPGNNModel
+from .censored_gaussian_process import CensoredEMGPModel, EMResult
 
-__all__ = ['DummyModel', 'GPModel', 'GPLLModel', 'CachedGPLLModel', 'RFModel', 'GPGKModel', 'GPGNNModel']
+__all__ = ['DummyModel', 'GPModel', 'GPLLModel', 'CachedGPLLModel', 'RFModel', 'GPGKModel', 'GPGNNModel', 'CensoredEMGPModel', 'EMResult']
